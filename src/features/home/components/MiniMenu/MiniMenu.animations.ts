60bd44235cbad037d19e3079a544321c3cd5a19e
Johan Petrikovsky <johan.petrikovsky@new-immo-group.com>
2022-08-23 19:31:21 +0200

    style: :dizzy: update section hero animations

--- src/features/home/components/MiniMenu/MiniMenu.animations.ts
+++ src/features/home/components/MiniMenu/MiniMenu.animations.ts
@@ -18,3 +18,30 @@ export const revealResize = (element, scaleOrign = 1.1) => {
     }
   );
 };
+
+export const animateCategories = () => {
+  gsap.to('#mini-menu-presentation .categorie', {
+    opacity: 1,
+    stagger: {
+      each: 0.4,
+      ease: 'expo4.inOut',
+    },
+  });
+};
+
+export const animateSeparators = () => {
+  gsap.fromTo(
+    '#mini-menu-presentation .separator',
+    {
+      opacity: 0,
+      y: -100,
+    },
+    {
+      opacity: 1,
+      y: 0,
+      duration: 0.4,
+      ease: 'expo4.inOut',
+      delay: 0.35 * 5,
+    }
+  );
+};
