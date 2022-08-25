331200787bc32320a42c05fda4f9783d7a83caec
Johan Petrikovsky <johan.petrikovsky@new-immo-group.com>
2022-08-25 23:42:13 +0200

    feat: :package: add astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import image from '@astrojs/image';
 import react from '@astrojs/react';
+import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image(), react()],
+  integrations: [image(), react(), compress()],
 });
