5da422289a1df7f165e1ffc5893c028003e71406
Johan Petrikovsky <johan.petrikovsky@gmail.com>
2023-03-19 14:33:40 +0100

    build: :building_construction: remove compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import image from '@astrojs/image';
 import react from '@astrojs/react';
-import compress from 'astro-compress';
+// import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image(), react(), compress()],
+  // integrations: [image(), react(), compress()],
+  integrations: [image(), react()],
 });
