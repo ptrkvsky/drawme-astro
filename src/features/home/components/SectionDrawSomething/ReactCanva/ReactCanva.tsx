09d8203ff6a3ed23e8dc56c78ddc6ddf2d466867
Johan Petrikovsky <johan.petrikovsky@gmail.com>
2023-07-15 22:19:44 +0200

    chore: :arrow_up: upgrade sass

--- src/features/home/components/SectionDrawSomething/ReactCanva/ReactCanva.tsx
+++ src/features/home/components/SectionDrawSomething/ReactCanva/ReactCanva.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useRef } from 'react';
 import CanvasDraw from 'react-canvas-draw';
 import { useCanvasResize, useSendEmail } from './ReactCanva.hooks';
