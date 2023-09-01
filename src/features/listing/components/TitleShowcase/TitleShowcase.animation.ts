fb366f5dfa13d5626ed355e3b8899f11243073e2
Johan Petrikovsky <johan.petrikovsky@gmail.com>
2023-09-01 09:56:15 +0200

    feat: :dizzy: animate title showcase

---
+++ src/features/listing/components/TitleShowcase/TitleShowcase.animation.ts
@@ -0,0 +1,47 @@
+import config from "src/config";
+import { SplitText } from "gsap/SplitText";
+import gsap from "gsap";
+
+gsap.registerPlugin(SplitText);
+
+export function animateTitle() {
+  const showCaseTitle = document.querySelector("#showcase-title");
+  if (!showCaseTitle) {
+    console.log("now showcase");
+    return;
+  }
+
+  const splitSheep = new SplitText(
+    document.querySelectorAll("#showcase-title .word"),
+    {
+      type: `chars`,
+    }
+  );
+
+  gsap.set(splitSheep.chars, { yPercent: -103 });
+
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: showCaseTitle,
+        endTrigger: "footer",
+        markers: config.mode === "development",
+        start: "top 75%",
+      },
+    })
+    .to(splitSheep.chars, {
+      duration: 1,
+      yPercent: 0,
+      stagger: 0.04,
+      ease: "expo.inOut",
+      onComplete: () => {
+        const event = new Event("titleShowcaseAnimationComplete");
+        document.dispatchEvent(event);
+      },
+    })
+    .to(".section__year", {
+      opacity: 1,
+      duration: 0.7,
+      delay: -0.25,
+    });
+}
