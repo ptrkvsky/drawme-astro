bfb97d5faebf5210b0ef3906278496b905709174
Johan Petrikovsky <johan.petrikovsky@gmail.com>
2023-09-01 18:24:20 +0200

    feat: :dizzy: animate showcase row

---
+++ src/features/listing/components/SectionShowcaseRow/SectionShowcaseRow.animation.ts
@@ -0,0 +1,81 @@
+import gsap from "gsap";
+import { SplitText } from "gsap/SplitText";
+import config from "src/config";
+
+gsap.registerPlugin(SplitText);
+
+/**
+ * Initializes SplitText animation for an array of HTMLElements by splitting their text content into characters.
+ * The characters are animated with GSAP to apply a specific y-axis transformation.
+ *
+ * @param elements - An array of HTMLElements to be split and animated.
+ * @returns An array of GSAP animations targeting individual characters after the split.
+ */
+
+export function initSplit(elements: HTMLElement[]) {
+  const splitElements = new SplitText(elements, {
+    type: `chars`,
+  });
+
+  gsap.set(splitElements.chars, {
+    yPercent: -103,
+  });
+
+  return splitElements.chars;
+}
+
+export function animateRow(row: HTMLLinkElement) {
+  const selector = gsap.utils.selector(row);
+
+  const date: HTMLElement[] = selector(".row__header__date");
+  const name: HTMLElement[] = selector(".row__header__name");
+  const tagline: HTMLElement[] = selector(".row__header__tagline");
+
+  const nameChars = initSplit(name);
+  const dateChars = initSplit(date);
+  const taglineChars = initSplit(tagline);
+
+  const imagesRow = selector(".row__content-img");
+
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: row,
+        endTrigger: "footer",
+        markers: config.mode === "development",
+        start: "top 75%",
+      },
+    })
+    // Date
+    .to(dateChars, {
+      duration: 1,
+      yPercent: 0,
+      stagger: 0.08,
+      ease: "expo.inOut",
+    })
+    // Name
+    .to(nameChars, {
+      duration: 0.5,
+      yPercent: 0,
+      stagger: nameChars.length / 1000,
+      ease: "expo.inOut",
+      delay: -0.5,
+    })
+    // Images projects
+    .to(imagesRow, {
+      xPercent: 0,
+      autoAlpha: 1,
+      stagger: 0.1,
+      duration: 1,
+      ease: "expo.inOut",
+      delay: -1,
+    })
+    // Tagline
+    .to(taglineChars, {
+      duration: 0.5,
+      yPercent: 0,
+      stagger: taglineChars.length / 2000,
+      ease: "expo.inOut",
+      delay: -0.5,
+    });
+}
---
+++ src/features/listing/components/SectionShowcaseRow/index.ts
@@ -0,0 +1,1 @@
+export { default as SectionShowcaseRow } from "./SectionShowcaseRow.astro";
