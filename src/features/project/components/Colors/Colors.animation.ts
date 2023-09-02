355db0c0ca7cebc8b675cd7035cbbcab080319b1
Johan Petrikovsky <johan.petrikovsky@gmail.com>
2023-09-02 20:39:52 +0200

    docs: :memo: add function documentation

--- src/features/project/components/Colors/Colors.animation.ts
+++ src/features/project/components/Colors/Colors.animation.ts
@@ -1,17 +1,27 @@
-import gsap from 'gsap';
+import gsap from "gsap";
 
+/**
+ * Function to reveal colors using GSAP animation.
+ */
 export function revealColors() {
+  // Select all HTML elements with the class 'color-item'
   const colorsElement: NodeListOf<HTMLDivElement> =
-    document.querySelectorAll('.color-item');
+    document.querySelectorAll(".color-item");
+
+  // Check if any elements with the class 'color-item' were found
   if (!colorsElement) return;
 
+  // Set initial opacity of color elements to 0
   gsap.set(colorsElement, {
     opacity: 0,
   });
 
-  document.addEventListener('presentationClientAnimationComplete', () => {
-    const tlRevealColors = gsap.timeline(); // New timeline for the second animation
+  // Listen for the 'presentationClientAnimationComplete' event
+  document.addEventListener("presentationClientAnimationComplete", () => {
+    // Create a new GSAP timeline for the second animation
+    const tlRevealColors = gsap.timeline();
 
+    // Animate the color elements
     tlRevealColors.fromTo(
       colorsElement,
       {
@@ -24,8 +34,9 @@ export function revealColors() {
         duration: 0.5,
         stagger: 0.1,
         onComplete: () => {
+          // Dispatch a custom event when the animation is complete
           const animationCompleteEvent = new Event(
-            'revealColorsAnimationComplete'
+            "revealColorsAnimationComplete"
           );
           document.dispatchEvent(animationCompleteEvent);
         },
