24563b74ed49f75ff595edf27b03fae5683099a8
Johan Petrikovsky <johan.petrikovsky@gmail.com>
2023-09-23 14:13:37 +0200

    fix: :dizzy: datalag

--- src/features/listing/animations.ts
+++ src/features/listing/animations.ts
@@ -1,6 +1,6 @@
-import { gsap } from 'gsap';
-import Lenis from '@studio-freight/lenis';
-import { preloadImages } from '../../features/listing/utils';
+import { gsap } from "gsap";
+import Lenis from "@studio-freight/lenis";
+import { preloadImages } from "../../features/listing/utils";
 
 interface DOM {
   sections: {
@@ -20,7 +20,7 @@ let lenis;
 // Initialize Lenis smooth scrolling
 function initSmoothScrolling() {
   lenis = new Lenis({
-    lerp: 0.075,
+    lerp: 0.085,
     smoothWheel: true,
   });
   const scrollFn = (time) => {
@@ -36,19 +36,19 @@ function scroll(DOM: DOM) {
     .timeline({
       scrollTrigger: {
         start: 0,
-        end: 'max',
+        end: "max",
         scrub: true,
       },
     })
-    .addLabel('start', 0)
+    .addLabel("start", 0)
     .to(
       DOM.sections.columns,
       {
-        ease: 'none',
+        ease: "none",
         startAt: { scale: 1.1 },
         scale: 1,
       },
-      'start'
+      "start"
     )
     .to(
       DOM.sections.columns,
@@ -56,10 +56,10 @@ function scroll(DOM: DOM) {
         scrollTrigger: {
           trigger: DOM.sections.showcase,
           start: 0,
-          end: 'top top',
+          end: "top top",
           scrub: true,
         },
-        ease: 'power4.inOut',
+        ease: "power4.inOut",
         startAt: {
           opacity: 0.2,
         },
@@ -68,15 +68,15 @@ function scroll(DOM: DOM) {
         yoyo: true,
         repeat: 1,
       },
-      'start'
+      "start"
     )
     .to(
       DOM.columnWraps,
       {
-        ease: 'none',
+        ease: "none",
         yPercent: (pos) => (pos % 2 ? 3 : -3),
       },
-      'start'
+      "start"
     );
 }
 
@@ -85,20 +85,20 @@ export function initListingAnimations() {
 
   const DOM = {
     sections: {
-      columns: document.querySelector('.section--columns'),
-      showcase: document.querySelector('.section--showcase'),
+      columns: document.querySelector(".section--columns"),
+      showcase: document.querySelector(".section--showcase"),
     },
-    columns: document.querySelectorAll('.section--columns > .columns'),
-    columnWraps: document.querySelectorAll('.section--columns .column-wrap'),
+    columns: document.querySelectorAll(".section--columns > .columns"),
+    columnWraps: document.querySelectorAll(".section--columns .column-wrap"),
     itemsWrappers: document.querySelectorAll(
-      '.section--columns .column-wrap .column'
+      ".section--columns .column-wrap .column"
     ),
-    items: document.querySelectorAll('.section--columns .column__item'),
-    images: document.querySelectorAll('.section--columns .column__item-img'),
+    items: document.querySelectorAll(".section--columns .column__item"),
+    images: document.querySelectorAll(".section--columns .column__item-img"),
   };
 
   // Preload images
-  preloadImages('.column__item-img').then(() => {
+  preloadImages(".column__item-img").then(() => {
     // Lenis (smooth scrolling)
     initSmoothScrolling();
     // GSAP Scroll Triggers
