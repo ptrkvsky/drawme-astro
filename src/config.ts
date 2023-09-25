ba72ddb80118232c8d4175e5f0c216a1cd9be105
Johan Petrikovsky <johan.petrikovsky@gmail.com>
2023-09-25 18:10:40 +0200

    fix: :bug: config mode

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,6 @@ const version = import.meta.env.PUBLIC_SANITY_API_VERSION;
 const dataSet = import.meta.env.PUBLIC_SANITY_DATASET;
 
 const config = {
-  // mode: import.meta.env.MODE,
   mode: "production",
   sanityGraphqlEndpoint: `https://${projectId}.api.sanity.io/${version}/graphql/${dataSet}/default`,
 };
